a7032957130451d559394a4f81d7ce0d9545b761
Mitchell <soggybag@gmail.com>
2018-12-21 10:03:46 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import inventory, { categories } from './inventory'
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Show products here</h1>
+
+        <ul>
+          {/* List product categories here */}
+        </ul>
+
+        <ul>
+          {/* Products listed here */}
+        </ul>
+
       </div>
     );
   }
 }
 
 export default App;
+
+// ==========================================================
+// Reduce inventory to count number of items in each category
+function countCategory(items, category) {
+  return items.reduce((total, item) => {
+    return item.category === category ? total += 1 : total
+  }, 0)
+}
+
+categories.forEach((cat) => {
+  console.log(cat, countCategory(inventory, cat))
+})
+
+// ==========================================================
+// Filter inventory on category
+
+function filterOnCategory(items, category) {
+  return items.filter((item) => {
+    return item.category === category
+  })
+}
+
+categories.forEach((cat) => {
+  console.log(cat, filterOnCategory(inventory, cat))
+})
+
+// ==========================================================
+// Map inventory from object to string of name and price
+
+function mapInventoryToString(items) {
+  return items.map((item) => {
+    return `${item.name} $${item.price}`
+  })
+}
+
+mapInventoryToString(inventory).forEach((item) => {
+  console.log(item)
+})
+
