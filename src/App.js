c698922c3829f8808bd9c7508754bbf569d947a3
LucHighwalker <luc.highwalker@gmail.com>
2019-01-28 16:13:31 -0800

    - in class work.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import inventory, { categories } from './inventory'
+import inventory, { categories } from './inventory';
 import './App.css';
 
 class App extends Component {
+  getCategories() {
+    return categories.map(cat => (
+      <span key={cat}>
+        <button>{cat}</button>
+      </span>
+    ));
+  }
+
+  getInventory() {
+    return inventory.map(item => (
+      <div key={item.id}>
+        <h1>{item.name}</h1>
+        <p>{item.price}</p>
+      </div>
+    ));
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Show products here</h1>
 
-        <ul>
-          {/* List product categories here */}
-        </ul>
+        <div>{this.getCategories()}</div>
 
-        <ul>
-          {/* Products listed here */}
-        </ul>
-
+        <div>{this.getInventory()}</div>
       </div>
     );
   }
